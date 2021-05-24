211b7b9d950774851c0ed10bc82ece72980db96d
KENGO NAKAMURA CHIBANA <kengonc98@gmail.com>
2021-05-23 21:50:11 -0400

    new datatable

--- resources/js/components/frmservicio.vue
+++ resources/js/components/frmservicio.vue
@@ -58,124 +58,50 @@
     <!-- Tabla de la lista de servicios-->
     <div id="servicio-table-main-content">
         <div>
-            Buscar: <input id="busquedaServicio" v-model="search" class="form-control" width="50px">
-            <button class="btn btn-primary" type="button" @click="filtrarServicios()">Mostrar</button>
+            <!-- Buscar: <input id="busquedaServicio" v-model="search" class="form-control" width="50px">
+            <button class="btn btn-primary" type="button" @click="filtrarServicios()">Mostrar</button> -->
                 
         </div>
-            <div>
-                <bootstrap-4-datatable :columns="columns" :data="rows" :filter="filter" :per-page="perPage">
-                </bootstrap-4-datatable>
-                <bootstrap-4-datatable-pager v-model="page" type="abbreviated"></bootstrap-4-datatable-pager>
-            </div>
+            <table id="tablaServicio">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Descripcion</th>
+                        <th>Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>algun nombre</td>
+                        <td>alguna desc</td>
+                        <td>25.00</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
 
 <script>
-
+import datatable from 'datatables.net-bs4'
 export default {
-    
     data(){
         return{
-            showModal: false,
-            ///Atributos del servicio
-            nombre :'',
-            descripcion : '',
-            precio : '',
-            search: '',
-            arrayServicio : [],
-            totalServicios: [],
-            //////////////
-
-            /////Necesario para Data Table
-            columns: [{
-                    label: 'id',
-                    field: 'id'
-                },
-                {
-                    label: 'nombre',
-                    field: 'nombre'
-                },
-                {
-                    label: 'descripcion',
-                    field: 'descripcion'
-                },
-                {
-                    label: 'precio',
-                    field: 'precio'
-                }
-            ],
-            rows: [],
-            page: 1,
-            filter:  '',
-            perPage: 12
-            /////
-            
+            showModal: false
         }
+    },
+    mounted(){
+        this.tabla()
     },
     methods:{
         closeModal(){
             this.showModal = false;
         },
-        ////Aca mantengo rows que es el total y solo anhado lo que si coinciden al arrayServicios
-        filtrarServicios(){
-            let me = this;
-            let search= this.search;
-            var i;
-            if(search==''){
-                this.rows=me.arrayServicio;
-            }else{
-                this.rows=[];
-                for (i=0; i<me.arrayServicio.length; i++){
-                    var servicio = me.arrayServicio[i];
-                    if(
-                        servicio.nombre.toLowerCase().indexOf(search) != -1 || 
-                        servicio.descripcion.toLowerCase().indexOf(search) != -1 || 
-                        servicio.precio.toString().indexOf(search) != -1 
-                    ){
-                        this.rows.push(servicio);
-                    }
-                }
-            }
-
-            //me.listar();
-        },
-        //Muestra en el cuadro el arrayServicios
-        listar(){
-            let me = this;
-            me.rows = me.arrayServicio;
-        },
-        guardar(){
-            let me = this;
-            if(me.nombre =='' || me.descripcion =='' || me.precio=='')
-                alert("Debe Llenar el formulario");
-            else
-            axios.post('request/nuevo-servicio',{
-                'nombre': this.nombre,
-                'descripcion': this.descripcion,
-                'precio': this.precio
-            }).then(function(error){
-                me.listar();
-            }).catch(function(error){
-                console.log(error);
-            });        
-        },
-        getTodo: function () {
-            axios.get('/listaServicios').then(function (res) {
-                this.arrayServicio = res.data;
-            }.bind(this));
-        },
-        showTodo: function () {
-                this.rows = this.arrayServicio ;
-        },
-    },
-    mounted() {
-        this.getTodo();
-        // document.getElementById("itabla").style.display="none";
-        // document.getElementById("bguardar").style.display="none";
-    },
-    
+        tabla(){
+            this.$nextTick(()=>{
+                $('#tablaServicio').DataTable();
+            });
+        }
+    }
 }
-
-
 </script>
