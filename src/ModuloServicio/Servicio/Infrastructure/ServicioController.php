940c5921e8d6c70c5bb0fc8110a8daab58c8cc31
KENGO NAKAMURA CHIBANA <kengonc98@gmail.com>
2021-05-30 00:19:54 -0400

    get especialidades with servicios ok

--- src/ModuloServicio/Servicio/Infrastructure/ServicioController.php
+++ src/ModuloServicio/Servicio/Infrastructure/ServicioController.php
@@ -104,6 +104,42 @@ class ServicioController
         return $arrayServicio;
     }
 
+    public function getAllServiciosWithEspecialides(Request $request){
+        $getServiciosUseCase = new GetServicioUseCase($this->servicioRepository);
+        $servicio = $getServiciosUseCase->__invoke();
+
+       // $getEspecialidadServicio = new GetEspecialidadServicioByServicioIdUseCase($this->especialidadRepository);
+       // $esp = $getEspecialidadServicio->__invoke($idServicio);
+
+       $arrayTodoLosServicios = array();
+       $arrayServicios = array();
+       $espeArray = array();
+       foreach($servicio as $serv){
+            $getEspecialidadServicio = new GetEspecialidadServicioByServicioIdUseCase(new EloquentEspecialidadServicioRepository);
+            $esp = $getEspecialidadServicio->__invoke($serv->id);
+            $arrayServicio = array(
+                'id' => $serv->id,
+                'nombre' => $serv->nombre,
+                'descripcion'=>$serv->descripcion,
+                'precio'=>$serv->precio
+            );
+            $eArray = array();
+            foreach($esp as $e){
+                $aux['idEspecialidad'] = $e->idEspecialidad;
+                $aux['nombre'] = $e->nombre;
+                $aux['descripcion'] = $e->descripcion;
+                array_push($eArray, $aux); 
+            }
+            $arrayServicio['arrayEspecialidades'] = $eArray;
+            array_push($arrayTodoLosServicios, $arrayServicio);
+        }
+
+        return $arrayTodoLosServicios;
+
+
+        return $arrayServicio;
+    }
+
     public function deleteServicio(Request $request){
         //falta
     }
